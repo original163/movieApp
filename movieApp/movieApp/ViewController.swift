5f05e831d768cedf08880404a74de8ed1d47bf83
original163 <original163@mail.ru>
2022-08-19 22:49:16 +0400

    - удален StoryBoard - добавлена иконка приложения

---
+++ movieApp/movieApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  movieApp
+//
+//  Created by Денис Денисов on 19.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
